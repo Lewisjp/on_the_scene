34f2e005282cabbd430482185b3bc063b5eaa4d7
LewisJPaul <lewis.jp@gmail.com>
2014-04-20 18:33:19 -0400

    updated views

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
 
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-OnTheScene::Application.config.secret_key_base = '94da48b463f6b3f6fff27947d0d40b496b988331a5d0b9a1483b74322666024d73de63458a6a9696730f8fc40d6f1cd82bb614d509e756ada214d40751644774'
+OnTheScene::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
+
+
+# pusher.app_id = ENV["PUSHER_APP_ID"]
+#pusher.key = ENV["PUSHER_KEY"]
+#pusher.secret = ENV["PUSHER_SECRET"]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420214843) do
+ActiveRecord::Schema.define(version: 20140420221120) do
+
+  create_table "geo_scenes", force: true do |t|
+    t.integer  "scene_id"
+    t.integer  "geolocation_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "geolocations", force: true do |t|
     t.string   "address"
