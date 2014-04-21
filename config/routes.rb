085861cc84aa2d1b74cd30e1b734366b2edf0ccf
LewisJPaul <lewis.jp@gmail.com>
2014-04-21 07:09:38 -0400

    updated pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 OnTheScene::Application.routes.draw do
-resources :geolocations do
-    resources :scenes, only: [:new, :create, :destroy, :edit, :update]
-  end
-
-  resources :scenes, only: [:show]
+  resources :geolocations 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -13,7 +9,7 @@ resources :geolocations do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+    get 'about' => 'scenes#about'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
